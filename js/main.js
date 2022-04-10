2504365c42ca72fe5a7cd6242bbadf4a53d884cb
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2022-04-10 16:56:43 +0530

    implemented loader

--- js/main.js
+++ js/main.js
@@ -46,6 +46,12 @@ $(document).ready(function() {
     }
   }
 
+  function hideLoader() {
+    $('#loading').hide();
+  }
+
+  $(window).ready(hideLoader);
+  setTimeout(hideLoader, 20*1000);
   // ========================================================================= //
   //  //NAVBAR SHOW - HIDE
   // ========================================================================= //
